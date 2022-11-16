c65b3e3b2a67a193a4af61c876166bd7fbf19f97
Virus <amitkeshri003@gmail.com>
2022-11-16 23:57:17 +0530

    added form validation in contact us

--- js/index.js
+++ js/index.js
@@ -10,11 +10,9 @@ jQuery(document).ready(function(){
         }
         i++;
     }, 1000);
+
     mobile_menu_init();
-    
-
 });
-
 
 
 jQuery(document).on('click', '.ham_burg_menu', function(){
@@ -30,23 +28,15 @@ jQuery(window).resize(function(){
 function mobile_menu_init() {
     if (window.matchMedia('(max-width: 480px)').matches) {
         jQuery('ul.navbar').hide();
-        jQuery('ul.navbar').before('<button class="ham_burg_menu"><img src="img/ham.png" /></button>');
         console.log('This is Mobile');
+        jQuery('ul.navbar').before('<button class="ham_burg_menu"><img src="img/ham.png"/></button>');
     } else {
         jQuery('ul.navbar').show();
         jQuery('.ham_burg_menu').remove();
         console.log('This is not Mobile');
     }
-
-};
-
-function validateForm() {
-  var x = document.forms["myForm"]["name"].value;
-  if (x == "" || x == null) {
-    alert("Name must be filled out");
-    return false;
-  }
 }
+
 
 function contact_us_accordion() {
     console.log('Accrodion Scripts');
@@ -55,8 +45,44 @@ function contact_us_accordion() {
 }
 
 
-
 jQuery(document).on('click', '.accordion_section h3', function(){
     jQuery(this).parent('div').find('p').slideToggle();
 });
 
+
+jQuery(document).on('click','.contact_submit', function(event){
+    event.preventDefault();
+    console.log("preventing from form submission");
+
+    var name = jQuery('#name').val();
+    var email = jQuery('#email').val();
+    var phone = jQuery('#phone').val();
+
+    jQuery(".error").remove();
+    if(name.length==0 || email.length==0|| phone.length==0){
+        alert("field the blank fields");
+    }
+    if(name.length < 2){
+        // alert("enter the valid number");
+        jQuery('#name').after('<span class="error">This field is required</span>');
+    }
+
+    if(email.length < 2){
+        jQuery('#email').after('<span class="error">This field is required</span>');
+    } else{
+        var regex_email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        var validEmail = regex_email.test(email);
+        if(!validEmail){
+            $('#email').after('<span class="error">Enter a valid email</span>');
+        }
+    }
+    if (phone.length!=10){
+        jQuery('#phone').after('<span class="error">This field is required</span>');
+    } else {
+        var regex_phone =  /^(?:(?:\+|0{0,2})91(\s*|[\-])?|[0]?)?([6789]\d{2}([ -]?)\d{3}([ -]?)\d{4})$/;
+        var valid_phone = regex_phone.test(phone);
+        if(!valid_phone){
+            jQuery('#phone').after('<span class="error">Enter a valid phone number</span>');
+        }
+    }
+});
